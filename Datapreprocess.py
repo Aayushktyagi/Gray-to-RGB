9e7b67530262297942b01703a4837496d12e6975
Aayushktyagi <aayush16081@iiitd.ac.in>
2019-04-06 09:33:41 +0530

    Data processing function

---
+++ Datapreprocess.py
@@ -0,0 +1,34 @@
+'''
+load rgb images and return dataset containing
+rgb and gray scale images in (n,x,y,channel) format
+'''
+
+
+import numpy as np
+import cv2
+import glob
+import os
+import matplotlib.pyplot as plt
+
+#load datset
+def getData():
+  rgb_list = []
+  gray_list = []
+  filepath = '/media/aayush/Work/Work/Github/Generative_models/Autoencoders/ImageColourization/flower-color-images'
+  file_path = glob.glob(os.path.join(filepath,'*.png'))
+
+  for img_path  in file_path:
+    rgb_img = cv2.imread(img_path)
+    gray_img = cv2.cvtColor(rgb_img, cv2.COLOR_BGR2GRAY)
+    rgb_img = cv2.resize(rgb_img,(224,224))
+    gray_img = cv2.resize(gray_img,(224,224))
+    rgb_list.append(rgb_img)
+    gray_list.append(gray_img)
+
+  rgb_images = np.stack(rgb_list,axis=0)
+  gray_images = np.stack(gray_list,axis=0)
+  print(np.shape(rgb_images),np.shape(gray_images))
+  return rgb_images , gray_images
+
+#rgb_images , gray_images = getData()
+#print(np.shape(rgb_images),np.shape(gray_images))
